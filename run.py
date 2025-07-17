4d1d14115534544ce75fe945512688f115cfbeee
Kimaiyoo <naomikimaiyo@kabarak.ac.ke>
2025-07-17 16:20:05 +0300

    minor fixes

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
+import os
 from app import create_app
+from app.extensions import db
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     app.run(debug=True)
+
