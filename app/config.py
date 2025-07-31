996416179aa6364e746aacb9f76a53ddf589b52c
chegeMP <mmureithi@kabarak.ac.ke>
2025-07-31 13:35:47 +0300

    Merged conflicts

--- app/config.py
+++ app/config.py
@@ -5,5 +5,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI") or 'postgresql://postgres:lifeisgood@localhost/Disetech'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
-    WEATHER_API_KEY = os.getenv("WEATHER_API_KEY") or 'c665a58919b94bc9b5893015251507'
+    WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
